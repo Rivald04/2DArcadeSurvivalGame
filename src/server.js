5daf9fedf0ce08a3f4719ec852397c22e2bc8a1b
Rivald04 <rivaldiaw04@gmail.com>
2024-03-30 10:06:20 +0700

    First Commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// import modul yang digunakan
+import express from 'express'
+import * as dotenv from 'dotenv' //untuk env
+import logs from './middleware/logs.js' //
+import authRoute from './route/authRoute.js'
+import highscoreRoute from './route/highscoreRoute.js'
+
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware 
+app.use(express.json());  //berupa json
+app.use(express.urlencoded({ extended: false }));  //berupa url decoded
+app.use(logs)
+
+// routes
+app.use(authRoute)
+app.use(highscoreRoute)
+
+////////////
+app.listen(`${port}`, () => {
+    console.log(`Server berjalan di port ${port}`)
+})
